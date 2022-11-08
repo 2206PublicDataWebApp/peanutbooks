9af9fbbc4711369ed9a7d8a7bb72e38c2a29c917
yangy <yangy@DESKTOP-6RHR9J6>
2022-11-08 10:34:32 +0900

    양유진 221108 오전 커밋
    - 알림 화면 수정
    - 회차 승인 시 해당 도서 알림 등록되도록 함

--- src/main/java/com/books/peanut/news/domain/News.java
+++ src/main/java/com/books/peanut/news/domain/News.java
@@ -1,5 +1,54 @@
 package com.books.peanut.news.domain;
 
 public class News {
-
+	private int newsNo; // 알림 번호
+	private String memberId; // 회원 아이디
+	private String newsContents; // 알림 내용
+	private int refBookNo; // 참고 도서 번호
+	private String readYN; // 알림 읽음 여부
+	private String newsType; // 알림 종류
+	
+	public int getNewsNo() {
+		return newsNo;
+	}
+	public void setNewsNo(int newsNo) {
+		this.newsNo = newsNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getNewsContents() {
+		return newsContents;
+	}
+	public void setNewsContents(String newsContents) {
+		this.newsContents = newsContents;
+	}
+	public int getRefBookNo() {
+		return refBookNo;
+	}
+	public void setRefBookNo(int refBookNo) {
+		this.refBookNo = refBookNo;
+	}
+	public String getReadYN() {
+		return readYN;
+	}
+	public void setReadYN(String readYN) {
+		this.readYN = readYN;
+	}
+	public String getNewsType() {
+		return newsType;
+	}
+	public void setNewsType(String newsType) {
+		this.newsType = newsType;
+	}
+	
+	@Override
+	public String toString() {
+		return "News [newsNo=" + newsNo + ", memberId=" + memberId + ", newsContents=" + newsContents + ", refBookNo="
+				+ refBookNo + ", readYN=" + readYN + ", newsType=" + newsType + "]";
+	}
+	
 }
